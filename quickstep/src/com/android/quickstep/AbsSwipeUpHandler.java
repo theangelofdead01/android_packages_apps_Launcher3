a7e23716942857745e1342d9cb8c8adca89c86af
hydrangea07 <104114625+hydrangea07@users.noreply.github.com>
2022-11-23 23:03:33 +0800

    clear task snapshots remnants on 'onRecentsAnimationComplete'
    - to avoid remnants on gesture start/swipe up

--- quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
+++ quickstep/src/com/android/quickstep/AbsSwipeUpHandler.java
@@ -432,7 +432,9 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
                     mRecentsView.onRecentsAnimationComplete();
                     if (mRecentsAnimationController != null) {
                         mRecentsAnimationController.cleanupScreenshot();
-                    } else if (mDeferredCleanupRecentsAnimationController != null) {
+                        mRecentsAnimationController = null;
+                    }
+                    if (mDeferredCleanupRecentsAnimationController != null) {
                         mDeferredCleanupRecentsAnimationController.cleanupScreenshot();
                         mDeferredCleanupRecentsAnimationController = null;
                     }
@@ -1942,7 +1944,6 @@ public abstract class AbsSwipeUpHandler<T extends StatefulActivity<S>,
         if (!controller.getFinishTargetIsLauncher()) {
             setDividerShown(true /* shown */, false /* immediate */);
         }
-        mRecentsAnimationController = null;
         mRecentsAnimationTargets = null;
         if (mRecentsView != null) {
             mRecentsView.setRecentsAnimationTargets(null, null);
